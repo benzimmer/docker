ac2caf791b93120b86cd85799f46194e7b712a25
Ben Zimmer <benjamin.zimmer@infopark.de>
2015-09-16 11:45:36 +0200

    ssl-terminator: don't run hitch workers as root

--- ssl-terminator/run.sh
+++ ssl-terminator/run.sh
@@ -4,4 +4,4 @@ echo "$CERT" > /tmp/cert.pem
 
 nginx
 
-hitch-openssl --backend=$BACKEND --frontend=$FRONTEND --ciphers="$CIPHERS" /tmp/cert.pem
+hitch-openssl --user=nobody --backend=$BACKEND --frontend=$FRONTEND --ciphers="$CIPHERS" /tmp/cert.pem
